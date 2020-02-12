0eb5464a5a7a5efc7f057ccbb1c534ad701b9d20
Iulian <iulian.gavrilov@elecnor.es>
2020-02-12 16:39:15 +0200

    Simple py to read the electrical consumtion

---
+++ application.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# __author__ = "Ronie Martinez"
+# __copyright__ = "Copyright 2019, Ronie Martinez"
+# __credits__ = ["Ronie Martinez"]
+# __license__ = "MIT"
+# __maintainer__ = "Ronie Martinez"
+# __email__ = "ronmarti18@gmail.com"
+import json
+import random
+import time
+import csv
+from datetime import datetime
+
+from flask import Flask, Response, render_template
+
+def readJSONCSVData():
+	dbhour = {}
+	with open('data/ianuarie.csv', newline='') as csvfile:
+		dbcsv = list(csv.reader(csvfile, delimiter=';', quotechar='\"'));
+		
+		#"2018-12-20 14:00"
+
+		for row in dbcsv[1:]:
+			for count, item in list(enumerate(row))[1:]:
+				#print(count, item)
+				dt='{0}-{1}-{2} {3}:{4}'.format(row[0].split('.')[2],row[0].split('.')[1],row[0].split('.')[0],count-1,"00:00");
+				#print(dt," ", item);
+				dbhour[dt]=item.replace(",",".");
+	return json.dumps( [{'time': ora, 'value': dbhour[ora]} for ora in dbhour]);
+
+application = Flask(__name__)
+random.seed()  # Initialize the random number generator
+
+
+@application.route('/')
+def index():
+    return render_template('index.html')
+
+
+@application.route('/chart-data')
+def chart_data():
+	def generate_random_data():
+		while True:
+			ret_val = readJSONCSVData();
+			yield f"{ret_val}\n\n";
+			time.sleep(10)
+	return Response(generate_random_data(), mimetype='/event-stream')
+
+@application.route('/static-data')
+def static_data():
+	return Response(readJSONCSVData(), mimetype='application/json')
+
+if __name__ == '__main__':
+    #print(readJSONCSVData());
+    application.run(debug=True, threaded=True)
+    
+
